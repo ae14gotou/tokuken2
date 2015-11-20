82e0eaffb7998b46ef34bf25aba7b55fdeca26ef
syuhei gotou <ae14gotou@g.kumamoto-nct.ac.jp>
2015-11-20 14:33:49 +0900

    11/20 add new file :jsm_sample.py

---
+++ AntTree/jsm_sample.py
@@ -0,0 +1,18 @@
+﻿#株価取得 jsm sample
+#株価を取得しcsvで保存する
+
+import jsm
+import datetime
+
+#csvに保存
+q = jsm.QuotesCsv()
+#クラスに保存の場合
+#q= jsm.Quotes()
+
+start_date = datetime.date(2015,4,1)
+end_date = datetime.date(2015,5,1)
+
+#株価を取得しcsvに保存
+q.save_historical_prices("NTTData.csv", 9613, jsm.DAILY, start_date, end_date)
+#クラスに保存の場合
+#q.get_historical_prices(9613, jsm.DAILY, start_date, end_date)
